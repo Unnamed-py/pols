76883ef604a2496ff926056e783faa0e8803960c
Unnamed <68333826+Unnamed-py@users.noreply.github.com>
2022-09-17 07:08:51 +0000

    fix again

--- pols/settings.py
+++ pols/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'pols.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': _secret.get('DEFAULT_DATABASE', {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    })
+    }
 }
 
 
