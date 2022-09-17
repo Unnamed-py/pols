09cda508d487a71ee73c9c10ffa9eb27608304ec
Unnamed <68333826+Unnamed-py@users.noreply.github.com>
2022-09-17 07:08:04 +0000

    small fix

--- pols/settings.py
+++ pols/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(Path(__file__).resolve().parent / 'secret.json') as _secret_fp:
-    _secret = json.load(_secret_fp)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
